ca93a2c69095c5ffbf533a370f6aa49e9a9d6390
pds17100 <pds17100@naver.com>
2024-03-20 16:15:58 +0900

    섹션 2 도메인 분석 설계(엔티티 클래스 개발)

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //매핑된(외래키가있는곳을연관관계의주인으로정해야하므로)
+    private List<Order> orders = new ArrayList<>();
+}
