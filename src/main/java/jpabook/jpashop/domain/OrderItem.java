0160e121cd6bb792fd28b114cff8ea52b89b3667
pds17100 <pds17100@naver.com>
2024-03-25 16:50:49 +0900

    섹션 6 주문 도메인 개발(주문, 주문상품 엔티티 개발, 주문 리포지토리 개발, 주문 서비스 개발)

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,7 +2,9 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.*;
@@ -10,6 +12,7 @@ import static jakarta.persistence.FetchType.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -27,4 +30,30 @@ public class OrderItem {
 
     private int orderprice; //주문가격
     private int count; //주문수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderprice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderprice(orderprice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderprice() * getCount();
+    }
+
 }
