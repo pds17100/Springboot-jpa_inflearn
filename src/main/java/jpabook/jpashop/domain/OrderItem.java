466bdab4ba3c7cf3c5a5339240b0765d8d42ff7d
pds17100 <pds17100@naver.com>
2024-04-02 14:30:51 +0900

    섹션 7 웹 계층 개발(주문 목록 검색, 취소)

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -28,14 +28,14 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    private int orderprice; //주문가격
+    private int orderPrice; //주문가격
     private int count; //주문수량
 
     //==생성 메서드==//
-    public static OrderItem createOrderItem(Item item, int orderprice, int count) {
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
         OrderItem orderItem = new OrderItem();
         orderItem.setItem(item);
-        orderItem.setOrderprice(orderprice);
+        orderItem.setOrderPrice(orderPrice);
         orderItem.setCount(count);
 
         item.removeStock(count);
@@ -53,7 +53,7 @@ public class OrderItem {
      * 주문상품 전체 가격 조회
      */
     public int getTotalPrice() {
-        return getOrderprice() * getCount();
+        return getOrderPrice() * getCount();
     }
 
 }
