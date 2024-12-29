f8866abd877dcfefb70f4d0134222203007e6ac2
pds17100 <pds17100@naver.com>
2024-12-29 18:15:56 +0900

    회원 등록, 수정, 조회 API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +17,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member") //매핑된(외래키가있는곳을연관관계의주인으로정해야하므로)
     private List<Order> orders = new ArrayList<>();
 }
